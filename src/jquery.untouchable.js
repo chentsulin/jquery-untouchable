e8326f21a1988ec1260f073b72a4154e4f594d96
Leo Chen-Tsu_Lin <lin19890808@gmail.com>
2014-09-23 17:34:48 +0800

    add mouse event & position calc

--- src/jquery.untouchable.js
+++ src/jquery.untouchable.js
@@ -12,13 +12,31 @@
   var Untouchable = function(element, options) {
     this.options = options;
     this.$element = $(element);
-  }
+
+    this.$element.on('mousemove', this.away);
+  };
 
   //
   // DEFAULT SETTING
 
   Untouchable.DEFAULTS = {
+    distance: 100,
+    speed: 400
+  };
 
+  Untouchable.prototype.calculateDistance(elem, mouseX, mouseY) {
+    return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left + (elem.width() / 2)), 2) + Math.pow(mouseY - (elem.offset().top + (elem.height() / 2)), 2)));
+  }
+
+  Untouchable.prototype.away = function(e) {
+    var mouseX = e.pageX;
+    var mouseY = e.pageY;
+
+    var distance = this.calculateDistance($(this), mouseX, mouseY);
+
+    if (distance < this.options.distance) {
+      // away
+    }
   };
 
   //
